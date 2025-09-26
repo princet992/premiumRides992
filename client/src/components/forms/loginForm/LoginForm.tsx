945e2a9e7d4ac9b146e7afacff6b02921e510078
Prince Thakur <princethakurameotech@gmail.com>
2025-09-26 12:21:54 +0530

    remove google login

--- client/src/components/forms/loginForm/LoginForm.tsx
+++ client/src/components/forms/loginForm/LoginForm.tsx
@@ -8,9 +8,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setUserCredential } from "@/features/AuthSlice/AuthSlice";
 import { useState } from "react";
-import {  useLoginUserMutation } from "@/api/userApi/usersAPi";
+import { useLoginUserMutation } from "@/api/userApi/usersAPi";
 import { colors } from "@/lib/theme";
-import { GoogleLogin } from "@react-oauth/google";
 const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
   const dispatch = useDispatch();
   const [loginUser, { isLoading }] = useLoginUserMutation();
@@ -36,16 +35,6 @@ const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
       }, 2000);
     }
   };
-
- const handleLogin = async (credentialResponse: any) => {
-    console.log("Full response:", credentialResponse);
-
-     if (!credentialResponse.credential) {
-      console.error("Google login failed");
-      return;
-    }
-  }
-
 
 
   return (
@@ -79,7 +68,7 @@ const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
                     Login with GitHUb
                   </Button> */}
 
-                  {/* <Button variant="outline" className={`w-full ${colors.headerGradient}`} onError={handleError}>
+                  <Button variant="outline" className={`w-full ${colors.headerGradient}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                       <path
                         d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -87,9 +76,8 @@ const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
                       />
                     </svg>
                     Login with Google
-                  </Button> */}
-     <GoogleLogin onSuccess={handleLogin} onError={() => console.log("Login Failed")} />
-
+                  </Button>
+                  {/* <GoogleLogin onSuccess={handleLogin} onError={() => console.log("Login Failed")} /> */}
                 </div>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                   <span className="bg-card text-muted-foreground relative z-10 px-2">Or continue with</span>
